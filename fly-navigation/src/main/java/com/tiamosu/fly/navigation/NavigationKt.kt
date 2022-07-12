a094d02836fb4dc88e7b2e57ae9c945271ff8a6f
djy <137359049@qq.com>
2022-07-12 15:06:39 +0800

    1.新增 Navigation 扩展：startWithPopTo、pop、popTo

--- fly-navigation/src/main/java/com/tiamosu/fly/navigation/NavigationKt.kt
+++ fly-navigation/src/main/java/com/tiamosu/fly/navigation/NavigationKt.kt
@@ -41,8 +41,17 @@ val View.navigator
             get() = findNavController()
     })
 
+
+/* ====================== 启动 Fragment start======================*/
+
 /**
+ * 启动新的 Fragment
  *
+ * @param resId 操作ID或要导航到的目标ID
+ * @param args 传递给目的地的参数
+ * @param interval 设置防抖间隔时间，单位毫秒
+ * @param navigatorExtras 额外内容传递给导航器，可配置页面共享元素等
+ * @param builder [NavOptionsBuilder] 配置
  */
 fun NavHostKt.start(
     @IdRes resId: Int,
@@ -51,7 +60,7 @@ fun NavHostKt.start(
     navigatorExtras: Navigator.Extras? = null,
     builder: NavOptionsBuilder.() -> Unit = {}
 ) {
-    navigate(
+    startNav(
         resId = resId,
         args = args,
         interval = interval,
@@ -61,20 +70,78 @@ fun NavHostKt.start(
 }
 
 /**
+ * 启动新的 Fragment，并关闭 [popUpToId] 之上的 Fragments
+ *
+ * @param resId 操作ID或要导航到的目标ID
+ * @param popUpToId 退出栈到指定目标ID
+ * @param inclusive 指定目标[popUpToId]是否也弹出
+ * @param args 传递给目的地的参数
+ * @param interval 设置防抖间隔时间，单位毫秒
+ * @param navigatorExtras 额外内容传递给导航器，可配置页面共享元素等
+ * @param builder [NavOptionsBuilder] 配置
+ */
+fun NavHostKt.startWithPopTo(
+    @IdRes resId: Int,
+    popUpToId: Int,
+    inclusive: Boolean = false,
+    args: Bundle? = null,
+    interval: Long = 500,
+    navigatorExtras: Navigator.Extras? = null,
+    builder: NavOptionsBuilder.() -> Unit = {}
+) {
+    startNav(
+        resId = resId,
+        popUpToId = popUpToId,
+        inclusive = inclusive,
+        args = args,
+        interval = interval,
+        builder = builder,
+        navigatorExtras = navigatorExtras
+    )
+}
+/* ====================== 启动 Fragment end======================*/
+
+
+/* ====================== 出栈 start======================*/
+
+/**
+ * 出栈当前 Fragment
+ */
+fun NavHostKt.pop() {
+    navigateUp()
+}
+
+/**
+ * 出栈 [popUpToId] 之上的 Fragments
+ *
+ * @param popUpToId 退出栈到指定目标ID
+ * @param inclusive 指定目标[popUpToId]是否也弹出
+ */
+fun NavHostKt.popTo(
+    @IdRes popUpToId: Int,
+    inclusive: Boolean = false
+) {
+    popBackStack(popUpToId, inclusive)
+}
+/* ====================== 出栈 end======================*/
+
+
+/**
  * Fragment页面跳转
  *
  * @param resId 操作ID或要导航到的目标ID
  * @param popUpToId 退出栈到指定目标ID
- * @param popUpToInclusive 给定的目的地是否也弹出
+ * @param inclusive 指定目标[popUpToId]是否也弹出
  * @param singleTop 是否进行栈顶复用
  * @param navigatorExtras 额外内容传递给导航器，可配置页面共享元素等
  * @param args 传递给目的地的参数
  * @param interval 设置防抖间隔时间，单位毫秒
+ * @param builder [NavOptionsBuilder] 配置
  */
-private fun NavHostKt.navigate(
+private fun NavHostKt.startNav(
     @IdRes resId: Int = -1,
     @IdRes popUpToId: Int = -1,
-    popUpToInclusive: Boolean = false,
+    inclusive: Boolean = false,
     singleTop: Boolean = false,
     navigatorExtras: Navigator.Extras? = null,
     args: Bundle? = null,
@@ -87,7 +154,7 @@ private fun NavHostKt.navigate(
             args = args,
             navOptions = navOptions {
                 anim { applyAnimDefault() }
-                popUpTo(popUpToId) { inclusive = popUpToInclusive }
+                popUpTo(popUpToId) { this.inclusive = inclusive }
                 launchSingleTop = singleTop
                 apply(builder)
             },
