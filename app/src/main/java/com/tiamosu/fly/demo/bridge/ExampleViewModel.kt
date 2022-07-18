813ab1fd20a488c225025890e73d493ce11e232a
djy <137359049@qq.com>
2022-07-18 12:35:42 +0800

    1.调试

--- app/src/main/java/com/tiamosu/fly/demo/bridge/ExampleViewModel.kt
+++ app/src/main/java/com/tiamosu/fly/demo/bridge/ExampleViewModel.kt
@@ -1,18 +1,14 @@
 package com.tiamosu.fly.demo.bridge
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.kunminx.architecture.ui.callback.UnPeekLiveData
 
 /**
  * @author ti
  * @date 2022/7/13.
  */
 class ExampleViewModel : ViewModel() {
-    val count by lazy { UnPeekLiveData<Int>() }
-
-    init {
-        count.value = 0
-    }
+    val count by lazy { MutableLiveData<Int>() }
 
     fun incrementCount() {
         count.value = (count.value ?: 0) + 1
